7c89b44bf017ad0739487290d06c405809121fec
Jari <jari@icecream>
2015-04-03 10:01:44 +0300

    added attack routine

--- automata.py
+++ automata.py
@@ -27,6 +27,7 @@ class organism():
         self.cells[y, x] = 1
         self.life[y, x] = random.random() * 10
         self.grow_chance = random.random() * 0.5 + 0.1
+        self.attack = random.random()
         self.term = term
         self.color = color
         self.last_update = time.time()
@@ -64,6 +65,25 @@ def fight(organisms, term):
     return targets
 
 
+def kill_cell(organisms, y, x):
+    for organism in organisms:
+        organism.cells[y, x] = 0
+
+
+def attack(organisms, term):
+    all_cells = get_all_cells(organisms, term)
+    for organism in organisms:
+        #cells = all_cells - organism.boundary
+        #cells = all_cells - get_boundary_cells(organisms, term)
+        cells = all_cells - organism.cells
+        #for cell in np.transpose(organism.boundary.nonzero()):
+        for cell in np.transpose(get_boundary_cells(organism, term).nonzero()):
+            neighbour = get_neighbours(cells < 1, cell[0], cell[1])
+            if neighbour and random.random() < organism.attack:
+                kill_cell(organisms, neighbour[0], neighbour[1])
+                organism.cells[neighbour[0], neighbour[1]] = 1
+
+
 def die(organisms):
     for organism in organisms:
         organism.life[organism.life > 0] -= time.time() - organism.last_update
@@ -80,7 +100,7 @@ def update(organisms, term):
             neighbours = get_neighbours(cells, cell[0], cell[1])
             if neighbours and random.random() < organism.grow_chance:
                 organism.cells[neighbours[0], neighbours[1]] = 1
-                organism.life[neighbours[0], neighbours[1]] = random.random() + 1
+                # organism.life[neighbours[0], neighbours[1]] = random.random() + 1
                 cells[neighbours[0], neighbours[1]] = 1
                 organism.last_update = time.time()
 
@@ -105,8 +125,8 @@ def get_neighbours(cells, y, x):
 def main():
     term = Terminal()
     specimens = []
-    N = 3
-    colors = [term.on_green, term.on_red, term.on_blue, term.on_yellow]
+    N = 7
+    colors = [term.on_green, term.on_red, term.on_blue, term.on_yellow, term.on_white, term.on_magenta, term.on_cyan]
     for idx in range(N):
         color = random.choice(range(len(colors)))
         specimens.append(organism(term, colors[color]))
@@ -115,7 +135,8 @@ def main():
         while True:
             update(specimens, term)
             draw2(specimens, term)
-            die(specimens)
+            # die(specimens)
+            attack(specimens, term)
             stdout.flush()
             time.sleep(0.5)
             clear(term, term.height)
