b62877beea75018e230d00eebf7393d0bdce7e58
Jari <jari@icecream>
2015-04-17 17:34:05 +0300

    added growth function for stackmata

--- stackmata.py
+++ stackmata.py
@@ -15,6 +15,7 @@ class Culture():
         self.attack = []
         self.color = []
         self.empty = True
+        self.all_cells = np.zeros((self.height, self.width), dtype=bool)
 
     def add_organism(self, seed):
         """ Add new organism to the stack """
@@ -23,11 +24,33 @@ class Culture():
 
         if self.empty:
             self.cells = new_cells
+            self.boundary = new_cells
+            self.all_cells = new_cells
             self.empty = False
         else:
             self.cells = np.dstack((self.cells, new_cells))
+            self.boundary = np.dstack((self.boundary, new_cells))
+            self.all_cells = np.sum(self.cells, axis=2)
 
         # Add parameters for new organism
         self.grow.append(random.random())
         self.attack.append(random.random())
         self.color.append((random.random(), random.random(), random.random()))
+
+    def update_all_cells(self):
+        self.all_cells = np.sum(self.cells, axis=2)
+
+    def get_boundary(self, cells):
+        """ Returns boundary cells of the input matrix. """
+        return cells - nd.binary_erosion(cells)
+
+    def growth(self):
+        """ Grows each organism according to grow chance. """
+        for c in range(self.boundary.shape[-1]):
+            area = nd.binary_dilation(self.cells[..., c]) - self.cells[..., c]
+            area = np.bitwise_and(area, np.invert(self.all_cells)).astype(float)
+            area[area > 0] = np.random.random(np.sum(area)) < self.grow[c]
+            self.cells[..., c] += area
+            self.boundary = (self.cells[..., c] -
+                             nd.binary_erosion(self.cells[..., c]))
+            self.update_all_cells()
