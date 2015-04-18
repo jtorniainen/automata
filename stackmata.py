e6af25422144eae8a6b70ed10e638db9c590fc94
Jari <jari@icecream>
2015-04-18 16:25:17 +0300

    started working on decay routine for stackmata

--- stackmata.py
+++ stackmata.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.ndimage as nd
 import random
+import time
 
 
 class Culture():
@@ -16,6 +17,7 @@ class Culture():
         self.color = []
         self.empty = True
         self.all_cells = np.zeros((self.height, self.width), dtype=bool)
+        self.life = []
         self.template = self.generate_template(64)
 
     def add_organism(self, seed):
@@ -36,9 +38,9 @@ class Culture():
         # Add parameters for new organism
         self.grow.append(random.random() * .5)
         self.attack.append(random.random())
-        self.color.append((random.randint(0, 255),
-                           random.randint(0, 10),
-                           random.randint(0, 10)))
+        self.color.append((random.randint(0, 50),
+                           random.randint(0, 255),
+                           random.randint(0, 50)))
 
     def generate_template(self, radius):
         """ Generates a circular template to determine valid growing areas. """
@@ -57,6 +59,10 @@ class Culture():
         """ Returns boundary cells of the input matrix. """
         return cells - nd.binary_erosion(cells)
 
+    def clear_cells(self, array):
+        for c in range(self.cells.shape[-1]):
+            self.cells[..., c] = self.cells[..., c] * np.invert(array)
+
     def growth(self):
         """ Grows each organism according to grow chance and template. """
         for c in range(self.boundary.shape[-1]):
@@ -72,17 +78,15 @@ class Culture():
 
     def decay(self):
         """ Removes expired cells. """
-        pass
-
-    def clear_cells(self, array):
-        for c in range(self.cells.shape[-1]):
-            self.cells[..., c] = self.cells[..., c] * np.invert(array)
+        self.life -= (time.time() - self.last_update)
+        self.clear_cells(np.sum(self.life < 0, axis=2))
+        self.update_all_cells()
 
     def fight(self):
         for c in range(self.cells.shape[-1]):
             area = np.bitwise_and(nd.binary_dilation(self.cells[..., c]) -
                                   self.cells[..., c],
-                                  self.all_cells)
+                                  self.all_cells - self.cells[..., c])
             area[area > 0] = np.random.random(np.sum(area)) < self.attack[c]
             self.clear_cells(area.astype(bool))
             self.cells[..., c] += area
